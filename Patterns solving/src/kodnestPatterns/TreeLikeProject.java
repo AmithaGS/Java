f460efe042abc79230450e3e38c72420ef31abfa
AmithaGS <amithags.xworkz@gmail.com>
2024-09-21 12:22:00 +0530

    patterns

---
+++ Patterns solving/src/kodnestPatterns/TreeLikeProject.java
@@ -0,0 +1,26 @@
+package kodnestPatterns;
+
+public class TreeLikeProject {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 7; i++) {
+			for (int j = 1; j <= (7-i); j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= (2*i)-1; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = 1; i <= 2; i++) {
+			for (int j = 1; j <= 5; j++) {
+					System.out.print(" ");
+			}
+			for (int k = 1; k <= 3; k++) {
+				System.out.print("*");
+			}
+				System.out.println();
+			}
+		}
+	}
+
